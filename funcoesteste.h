60d14ca760e41732eb3932212dfd803c31349083
Matheus Emanuel <c06mvs@gmail.com>
2018-05-27 13:31:18 -0300

    Codigo com erro nas funcoes

---
+++ funcoesteste.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#define tamanho 50
+
+
+
+  typedef struct{;
+
+    char time[30];
+    int codigo;
+    char ender[40];
+    char cidade[50];
+    char estado[40];
+    char ano[4];
+
+  }Times;
+
+Times menuTimes[tamanho];
